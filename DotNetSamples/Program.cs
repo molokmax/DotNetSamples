e6c8ddb5fbc0537d054bf23e625475a1a0268d80
Maxim Molokanov <maxim.molokanov@smart-com.su>
2020-04-11 20:47:25 +0300

    sample 1 and sample 2

---
+++ DotNetSamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DotNetSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sample1:");
+            Sample1.Sample.Do();
+
+            Console.WriteLine();
+            Console.WriteLine("Sample2:");
+            Sample2.Sample.Do();
+
+            Console.ReadKey();
+        }
+    }
+}
